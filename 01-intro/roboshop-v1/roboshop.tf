fc9b0a3e91a16b1595d9d5e58a7737c3a1f122f3
Naresh2129 <nareshboligorla@gmail.com>
2024-05-10 09:13:31 +0530

    ansible

--- 01-intro/roboshop-v1/roboshop.tf
+++ 01-intro/roboshop-v1/roboshop.tf
@@ -23,6 +23,15 @@ resource "aws_instance" "frontend" {
       Name = "catalogue"
     }
 
+    resource "aws_route53_record" "www" {
+      zone_id = aws_route53_zone.primary.zone_id
+      name    = "www.example.com"
+      type    = "A"
+      ttl     = 300
+      records = [aws_eip.lb.public_ip]
+    }
+
+
     resource "aws_instance" "cart" {
       ami           = ami-0f3c7d07486cad139
       instance_type = "t2.micro"
@@ -30,6 +39,14 @@ resource "aws_instance" "frontend" {
 
       tags = {
         Name = "cart"
+      }
+
+      resource "aws_route53_record" "www" {
+        zone_id = aws_route53_zone.primary.zone_id
+        name    = "www.example.com"
+        type    = "A"
+        ttl     = 300
+        records = [aws_eip.lb.public_ip]
       }
 
       resource "aws_instance" "mongodb" {
@@ -41,6 +58,14 @@ resource "aws_instance" "frontend" {
           Name = "mongodb"
         }
 
+        resource "aws_route53_record" "www" {
+          zone_id = aws_route53_zone.primary.zone_id
+          name    = "www.example.com"
+          type    = "A"
+          ttl     = 300
+          records = [aws_eip.lb.public_ip]
+        }
+
         resource "aws_instance" "mysql" {
           ami           = ami-0f3c7d07486cad139
           instance_type = "t2.micro"
@@ -50,6 +75,15 @@ resource "aws_instance" "frontend" {
             Name = "mysql"
           }
 
+
+          resource "aws_route53_record" "www" {
+            zone_id = aws_route53_zone.primary.zone_id
+            name    = "www.example.com"
+            type    = "A"
+            ttl     = 300
+            records = [aws_eip.lb.public_ip]
+          }
+
           resource "aws_instance" "rabbitmq" {
             ami           = ami-0f3c7d07486cad139
             instance_type = "t2.micro"
@@ -57,6 +91,15 @@ resource "aws_instance" "frontend" {
 
             tags = {
               Name = "rabbitmq"
+            }
+
+
+            resource "aws_route53_record" "www" {
+              zone_id = aws_route53_zone.primary.zone_id
+              name    = "www.example.com"
+              type    = "A"
+              ttl     = 300
+              records = [aws_eip.lb.public_ip]
             }
 
             resource "aws_instance" "payment" {
@@ -77,6 +120,14 @@ resource "aws_instance" "frontend" {
                   Name = "user"
                 }
 
+                resource "aws_route53_record" "www" {
+                  zone_id = aws_route53_zone.primary.zone_id
+                  name    = "www.example.com"
+                  type    = "A"
+                  ttl     = 300
+                  records = [aws_eip.lb.public_ip]
+                }
+
                 resource "aws_instance" "shipping" {
                   ami           = ami-0f3c7d07486cad139
                   instance_type = "t2.micro"
@@ -86,6 +137,14 @@ resource "aws_instance" "frontend" {
                     Name = "shipping"
                   }
 
+                  resource "aws_route53_record" "www" {
+                    zone_id = aws_route53_zone.primary.zone_id
+                    name    = "www.example.com"
+                    type    = "A"
+                    ttl     = 300
+                    records = [aws_eip.lb.public_ip]
+                  }
+
                   resource "aws_instance" "redis" {
                     ami           = ami-0f3c7d07486cad139
                     instance_type = "t2.micro"
@@ -93,4 +152,12 @@ resource "aws_instance" "frontend" {
 
                     tags = {
                       Name = "redis"
+                    }
+
+                    resource "aws_route53_record" "www" {
+                      zone_id = aws_route53_zone.primary.zone_id
+                      name    = "www.example.com"
+                      type    = "A"
+                      ttl     = 300
+                      records = [aws_eip.lb.public_ip]
                     }
