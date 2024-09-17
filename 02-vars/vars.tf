34dc6033a440fa612f598f8e6a5ca64ce481c3a7
naresh <nareshboligorla@gmail.com>
2024-09-17 13:03:56 +0530

    terraform variable

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -4,15 +4,32 @@ variable "fruits" {
 output "fruit_name" {
   value = var.fruits
 }
-variable "fruits_with_price" {
+variable "fruit_details" {
   default = {
-    apple = 100
-    banana = 55
-    oranges = 42
-    lemons = 28
+    apple = {
+      stock = 100
+      price = 25
+      sale  = true
+
+    }
+    banana ={
+      stock = 200
+      price = 24
+      sale  = true
+    }
+    oranges = {
+      stock = 300
+      price = 24
+      sale  = false
+    }
+    lemons = {
+      stock = 20
+      price = 20
+      sale  = true
+    }
 
   }
 }
 
 output "apple_price" {
-  value = var.fruits_with_price ["apple"]
+  value = "apple price" = $(var.fruit_details["apple"].stock)
