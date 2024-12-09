cd1826112b9a87c6d10b454905f3cd905107b631
naresh <nareshboligorla@gmail.com>
2024-12-09 15:34:16 +0530

    roboshop-v3 configuaration by using modules

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -1,24 +1,20 @@
 resource "aws_instance" "instance" {
-  for_each = var.components
-  ami           = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = lookup(each.value, "name", null)
+    Name = var.name
   }
 }
 
 resource "aws_route53_record" "record" {
-  for_each = var.components
+
   zone_id =  var.zone_id
-  name    = "var.instance-dev.nareshboligorla.shop"
+  name    = "${var.name}-dev.nareshboligorla.shop"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance, "private_ip"]
 }
-variable "components" {}
-variable "zone_id" {}
-variable "instance_type" {}
-variable "vpc_security_group_ids" {}
-variable "ami" {}
+
